2160b89209ea6e42549a4bf8e8afa51c8f297092
sajib689 <jibonraja11@gmail.com>
2023-08-17 16:23:31 +0600

    third commit update categories and news section in server side

--- index.js
+++ index.js
@@ -18,7 +18,16 @@ app.get('/news/:id', (req, res) => {
     const id = req.params.id
     const selectedNews = news.find(n => n._id === id)
     res.send(selectedNews)
-    console.log(id)
+})
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    if(id === 0) {
+        res.send(news)
+    } else{
+    const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+    res.send(categoryNews)
+    }
+    
 })
 app.listen(port, (req, res) => {
     console.log('listening on port', port)
