545e0e3367f04a9c5f9a575f48694cb3ea71cb7b
sajib689 <jibonraja11@gmail.com>
2023-08-17 11:23:54 +0600

    first

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const cors = require('cors')
 const port = 3000
 app.use(cors())
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 app.get('/', (req, res) => {
     res.send('Welcome to the server')
 })
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+    console.log(id)
 })
 app.listen(port, (req, res) => {
     console.log('listening on port', port)
